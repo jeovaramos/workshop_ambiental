735d7bbe5bc6b36a566001cbf328abc94208e246
Jeová Ramos <60404728+jeovaramos@users.noreply.github.com>
2023-12-14 22:24:07 -0300

    Aula 14/12/2023 (#1)
    * Workshop.
    
    * Presentation link.
    
    ---------
    
    Co-authored-by: Jeova Silva <jeova.silva@simepar.br>

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Me formei e agora?
 ## Ciência de dados ambientais
 
-
+Apresentação: [link](https://www.canva.com/design/DAF2qtCnZLY/fNYFaYEK7_B987MA5WvyzQ/edit?utm_content=DAF2qtCnZLY&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton).
--- app.py
+++ app.py
@@ -1,5 +1,44 @@
 """Launches the main streamlit application."""
 
+import pandas as pd
+import plotly.express as px
 import streamlit as st
 
-st.write("Hello, World.")
+st.set_page_config(
+    page_title="Me formei. E Agora?",
+    page_icon="🌎",
+)
+st.header("Hello, World!")
+
+data = pd.read_csv("data/estudante_escola.csv")
+data["estudante"] = data["estudante"].astype(str)
+data["escola"] = data["escola"].astype(str)
+
+# st.table(data.dtypes)
+st.dataframe(data.head(15))
+
+fig = px.histogram(
+    data, x="desempenho", marginal="rug", hover_data=data.columns, nbins=10
+)  # box or rug
+fig.update_layout(xaxis_range=[0, 100])
+st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+
+
+school = st.select_slider("Selecione a escola", options=data["escola"].unique())
+col1, col2 = st.columns(2)
+selection = data["escola"] == school
+fig1 = px.histogram(
+    data[selection], x="desempenho", marginal="rug", hover_data=data.columns, nbins=10
+)  # box or rug
+fig1.update_layout(xaxis_range=[0, 100])
+col1.plotly_chart(fig1, theme="streamlit", use_container_width=True)
+
+texp_mean = round(data[selection]["texp"].mean())
+fig2 = px.scatter(
+    data[selection], x="horas", y="desempenho", title=f"texp médio = {texp_mean}"
+)
+fig2.update_layout(yaxis_range=[0, 100])
+col2.plotly_chart(fig2, theme="streamlit", use_container_width=True)
+
+fig = px.scatter(data, x="escola", y="desempenho", size="horas", color="texp")
+st.plotly_chart(fig, theme="streamlit", use_container_width=True)
